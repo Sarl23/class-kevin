8cabae6ef0d13fa4fe63f4c8e0ad5fa29a0e25e7
Sarl23 <sergioandresrojasleon@gmail.com>
2025-03-08 17:08:49 -0500

    first commit

---
+++ class_25_Jan_2025.js
@@ -0,0 +1,88 @@
+// JS _ L Desarrollo de Application web
+
+// var, let, const
+var lastName = "Persona1"
+// var lastName = "Timmy" // numeros, valores.... 
+// lastName = ""
+
+// console.log(lastName); // mostramos por consola, el valor de cada variable,. const .. 
+
+
+// Tipos de Datos en JS
+
+// Número (number) = 1,, 0.3
+// Cadena caracteres (String)= "testing", "#$@#$", "11", 
+// Booleano.(Boolean) = true, false // 
+
+// Nulo() = null 
+// Indefinido() = undefined 
+
+
+
+// Operadores Aritmeticos
+
+// && = y , || = o , === ,
+//  !true , !false
+
+// Ejercio
+
+var name = "Persona1"
+console.log(name === lastName);
+
+
+
+// condicionales
+
+// if = Si = declarar 
+// elseif = 
+// else =
+
+var age = 12
+var nameAge = age + 50
+
+/*?
+else if(age > 12){
+    console.log("es mayor a cero") }
+else{
+    console.log('no es ninguna')
+}*/
+
+
+// Arreglo
+const array = [110, true, 'text',2,3,3,3];
+const array2 = [String] //Solo permite cosa/elementos solo strings 
+
+
+
+//[0,1,2,3,4 ....]
+const persona = ['Tmmy', 'lopex', 27, true]// Persona son datos base de una persona
+
+
+const carCopy = [];
+const model = 'Q3' //documentGetBYId ...
+carCopy.push(model)
+
+console.log(carCopy.length)
+
+for (let i = 0; i < carCopy.length; i++){
+    console.log('el valor del arreglon index',[i],'es', carCopy[i])
+    if(i ==1){
+        console.log('valor de index en el car',[i],'es', carCopy[i])
+    }
+}
+
+// Objeto 
+// [] = {}
+
+
+
+
+// Función
+
+
+
+
+// Condicionales 
+
+// Estructura de datos basica:
+
